2e6d8e19051fd5898867861d8ab7c3dd7839ad7c
TrollyLoki <trollyloki@factmc.net>
2019-09-14 19:57:22 -0400

    Change sql access method

--- src/main/java/net/factmc/FactAchievements/Achievements.java
+++ src/main/java/net/factmc/FactAchievements/Achievements.java
@@ -1,10 +1,8 @@
 package net.factmc.FactAchievements;
 
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
 import net.factmc.FactCore.FactSQLConnector;
@@ -17,63 +15,21 @@ public class Achievements {
 		if (progress.split("/").length == 2) return;
 		String name = FactSQLConnector.getName(uuid);
 		
-		boolean exists = false;
-		try {
-			
-			PreparedStatement statement = FactSQLConnector.getMysql().getConnection()
-					.prepareStatement("SELECT * FROM " + FactSQLConnector.getAchievementsTable() + " WHERE `UUID`=? AND `SERVER`=? AND `NAME`=?");
-			statement.setString(1, uuid.toString());
-			statement.setString(2, server);
-			statement.setString(3, achievement);
-			
-			ResultSet rs = statement.executeQuery();
-			if (rs.next()) exists = true;
-			
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
+		boolean exists = FactSQLConnector.getCount(FactSQLConnector.getAchievementsTable(), new String[] {"UUID", "SERVER", "NAME"},
+				new Object[] {uuid.toString(), server, achievement}) > 0;
 		
 		if (!exists) {
-			try {
-				
-				PreparedStatement insert = FactSQLConnector.getMysql().getConnection()
-						.prepareStatement("INSERT INTO " + FactSQLConnector.getAchievementsTable()
-						+ " (UUID,SERVER,NAME,PROGRESS) VALUE (?,?,?,?)");
-				insert.setString(1, uuid.toString());
-				insert.setString(2, server);
-				insert.setString(3, achievement);
-				insert.setString(4, progress);
-				insert.executeUpdate();
-				
-				System.out.println("[FactAchievements] Set progress of " + achievement +
-						" on " + server + " for " + name + " to " + progress);
-				
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
+			FactSQLConnector.insertRow(FactSQLConnector.getAchievementsTable(), new String[] {"UUID", "SERVER", "NAME", "PROGRESS"},
+					new Object[] {uuid.toString(), server, achievement, progress});
 		}
 	
 		else {
-			
-			try {
-				
-				PreparedStatement insert = FactSQLConnector.getMysql().getConnection()
-						.prepareStatement("UPDATE " + FactSQLConnector.getAchievementsTable()
-						+ " SET `PROGRESS`=? WHERE `UUID`=? AND `SERVER`=? AND `NAME`=?");
-				insert.setString(1, progress);
-				insert.setString(2, uuid.toString());
-				insert.setString(3, server);
-				insert.setString(4, achievement);
-				insert.executeUpdate();
-				
-				System.out.println("[FactAchievements] Set progress of " + achievement +
-						" on " + server + " for " + name + " to " + progress);
-				
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-			
+			FactSQLConnector.updateRow(FactSQLConnector.getAchievementsTable(), new String[] {"UUID", "SERVER", "NAME"},
+					new Object[] {uuid.toString(), server, achievement}, "PROGRESS", progress);
 		}
+		
+		System.out.println("[FactAchievements] Set progress of " + achievement +
+				" on " + server + " for " + name + " to " + progress);
 		
 	}
 	
@@ -87,23 +43,8 @@ public class Achievements {
 	
 	public static String getAchievement(UUID uuid, String server, String achievement) {
 		
-		try {
-			
-			PreparedStatement statement = FactSQLConnector.getMysql().getConnection()
-					.prepareStatement("SELECT * FROM " + FactSQLConnector.getAchievementsTable() + " WHERE `UUID`=? AND `SERVER`=? AND `NAME`=?");
-			statement.setString(1, uuid.toString());
-			statement.setString(2, server);
-			statement.setString(3, achievement);
-			
-			ResultSet rs = statement.executeQuery();
-			if (!rs.next()) return null;
-			return rs.getString("PROGRESS");
-			
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		
-		return null;
+		return FactSQLConnector.getValues(FactSQLConnector.getAchievementsTable(), new String[] {"UUID", "SERVER", "NAME"},
+				new Object[] {uuid.toString(), server, achievement}, "PROGRESS").toString();
 		
 	}
 	
@@ -113,22 +54,10 @@ public class Achievements {
 		
 		List<String[]> list = new ArrayList<String[]>();
 		
-		try {
-			
-			PreparedStatement statement = FactSQLConnector.getMysql().getConnection()
-					.prepareStatement("SELECT * FROM " + FactSQLConnector.getAchievementsTable() + " WHERE `UUID`=? AND `SERVER`=?");
-			statement.setString(1, uuid.toString());
-			statement.setString(2, server);
-			
-			ResultSet rs = statement.executeQuery();
-			while (rs.next()) {
-				String name = rs.getString("NAME");
-				String progress = rs.getString("PROGRESS");
-				list.add(new String[] {name, progress});	
-			}
-			
-		} catch (SQLException e) {
-			e.printStackTrace();
+		List<Map<String, Object>> rows = FactSQLConnector.getRows(FactSQLConnector.getAchievementsTable(), new String[] {"UUID", "SERVER"},
+				new Object[] {uuid.toString(), server});
+		for (Map<String, Object> row : rows) {
+			list.add(new String[] {row.get("NAME").toString(), row.get("PROGRESS").toString()});
 		}
 		
 		return list;
