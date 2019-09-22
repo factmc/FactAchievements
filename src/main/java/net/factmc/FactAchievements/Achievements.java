d5dbf333c2a0bc1e85dd502bc26f22a3de90f438
TrollyLoki <trollyloki@factmc.net>
2019-09-21 22:03:50 -0400

    FactCore Update

--- src/main/java/net/factmc/FactAchievements/Achievements.java
+++ src/main/java/net/factmc/FactAchievements/Achievements.java
@@ -5,46 +5,48 @@ import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
-import net.factmc.FactCore.FactSQLConnector;
+import net.factmc.FactCore.FactSQL;
 
 public class Achievements {
 	
 	// WILL ADD MORE THINGS NOT UPDATE
-	public static void updateAchievement(UUID uuid, String server, String achievement, String progress) {
+	public static boolean updateAchievement(UUID uuid, String server, String achievement, String progress) {
 		
-		if (progress.split("/").length == 2) return;
-		String name = FactSQLConnector.getName(uuid);
-		
-		boolean exists = FactSQLConnector.getCount(FactSQLConnector.getAchievementsTable(), new String[] {"UUID", "SERVER", "NAME"},
+		if (progress.split("/").length != 2) return false;
+		boolean exists = FactSQL.getInstance().count(FactSQL.getAchievementsTable(), "`UUID`=? AND `SERVER`=? AND `NAME`=?",
 				new Object[] {uuid.toString(), server, achievement}) > 0;
 		
+		boolean success = false;
 		if (!exists) {
-			FactSQLConnector.insertRow(FactSQLConnector.getAchievementsTable(), new String[] {"UUID", "SERVER", "NAME", "PROGRESS"},
+			success = FactSQL.getInstance().insert(FactSQL.getAchievementsTable(), new String[] {"UUID", "SERVER", "NAME", "PROGRESS"},
 					new Object[] {uuid.toString(), server, achievement, progress});
 		}
 	
 		else {
-			FactSQLConnector.updateRow(FactSQLConnector.getAchievementsTable(), new String[] {"UUID", "SERVER", "NAME"},
-					new Object[] {uuid.toString(), server, achievement}, "PROGRESS", progress);
+			success = FactSQL.getInstance().update(FactSQL.getAchievementsTable(), "PROGRESS", progress, "`UUID`=? AND `SERVER`=? AND `NAME`=?",
+					new Object[] {uuid.toString(), server, achievement});
 		}
 		
-		System.out.println("[FactAchievements] Set progress of " + achievement +
-				" on " + server + " for " + name + " to " + progress);
+		if (success) System.out.println("[FactAchievements] Set progress of " + achievement +
+					" on " + server + " for " + FactSQL.getInstance().getName(uuid) + " to " + progress);
+		else System.out.println("[FactAchievements] Failed to set progress of " + achievement +
+				" on " + server + " for " + FactSQL.getInstance().getName(uuid) + " to " + progress);
+		return success;
 		
 	}
 	
-	public static void grantAchievement(UUID uuid, String server, String achievement) {
-		updateAchievement(uuid, server, achievement, "1/1");
+	public static boolean grantAchievement(UUID uuid, String server, String achievement) {
+		return updateAchievement(uuid, server, achievement, "1/1");
 	}
-	public static void revokeAchievement(UUID uuid, String server, String achievement) {
-		updateAchievement(uuid, server, achievement, "0/1");
+	public static boolean revokeAchievement(UUID uuid, String server, String achievement) {
+		return updateAchievement(uuid, server, achievement, "0/1");
 	}
 	
 	
 	public static String getAchievement(UUID uuid, String server, String achievement) {
 		
-		return FactSQLConnector.getValues(FactSQLConnector.getAchievementsTable(), new String[] {"UUID", "SERVER", "NAME"},
-				new Object[] {uuid.toString(), server, achievement}, "PROGRESS").toString();
+		return FactSQL.getInstance().select(FactSQL.getAchievementsTable(), "PROGRESS", "`UUID`=? AND `SERVER`=? AND `NAME`=?",
+				new Object[] {uuid.toString(), server, achievement}).toString();
 		
 	}
 	
@@ -54,8 +56,8 @@ public class Achievements {
 		
 		List<String[]> list = new ArrayList<String[]>();
 		
-		List<Map<String, Object>> rows = FactSQLConnector.getRows(FactSQLConnector.getAchievementsTable(), new String[] {"UUID", "SERVER"},
-				new Object[] {uuid.toString(), server});
+		List<Map<String, Object>> rows = FactSQL.getInstance().select(FactSQL.getAchievementsTable(), new String[] {"NAME", "PROGRESS"},
+				"`UUID`=? AND `SERVER`=?", new Object[] {uuid.toString(), server});
 		for (Map<String, Object> row : rows) {
 			list.add(new String[] {row.get("NAME").toString(), row.get("PROGRESS").toString()});
 		}
