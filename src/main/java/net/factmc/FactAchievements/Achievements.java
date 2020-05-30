ac9127c5dce9028f23f71b05b9cee2ab922c97c5
TrollyLoki <trollyloki@factmc.net>
2020-05-30 16:04:45 -0400

    Update to work with new CompletableFutures system

--- src/main/java/net/factmc/FactAchievements/Achievements.java
+++ src/main/java/net/factmc/FactAchievements/Achievements.java
@@ -4,64 +4,73 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
 
 import net.factmc.FactCore.FactSQL;
 
 public class Achievements {
 	
 	// WILL ADD MORE THINGS NOT UPDATE
-	public static boolean updateAchievement(UUID uuid, String server, String achievement, String progress) {
+	public static CompletableFuture<Boolean> updateAchievement(UUID uuid, String server, String achievement, String progress) {
 		
-		if (progress.split("/").length != 2) return false;
-		boolean exists = FactSQL.getInstance().count(FactSQL.getAchievementsTable(), "`UUID`=? AND `SERVER`=? AND `NAME`=?",
-				new Object[] {uuid.toString(), server, achievement}) > 0;
+		if (progress.split("/").length != 2) return CompletableFuture.completedFuture(false);
 		
-		boolean success = false;
-		if (!exists) {
-			success = FactSQL.getInstance().insert(FactSQL.getAchievementsTable(), new String[] {"UUID", "SERVER", "NAME", "PROGRESS"},
-					new Object[] {uuid.toString(), server, achievement, progress});
-		}
-	
-		else {
-			success = FactSQL.getInstance().update(FactSQL.getAchievementsTable(), "PROGRESS", progress, "`UUID`=? AND `SERVER`=? AND `NAME`=?",
-					new Object[] {uuid.toString(), server, achievement});
-		}
-		
-		if (success) System.out.println("[FactAchievements] Set progress of " + achievement +
-					" on " + server + " for " + FactSQL.getInstance().getName(uuid) + " to " + progress);
-		else System.out.println("[FactAchievements] Failed to set progress of " + achievement +
-				" on " + server + " for " + FactSQL.getInstance().getName(uuid) + " to " + progress);
-		return success;
+		return FactSQL.getInstance().update(FactSQL.getAchievementsTable(), "PROGRESS", progress, "`UUID`=? AND `SERVER`=? AND `NAME`=?",
+				new Object[] {uuid.toString(), server, achievement}).thenCompose((result) -> {
+					
+					if (result <= 0) {
+						return FactSQL.getInstance().insert(FactSQL.getAchievementsTable(), new String[] {"UUID", "SERVER", "NAME", "PROGRESS"},
+								new Object[] {uuid.toString(), server, achievement, progress});
+					}
+					
+					else return CompletableFuture.completedFuture(result);
+					
+				}).thenApply((result) -> {
+					
+					if (result > 0) {
+						System.out.println("[FactAchievements] Set progress of " + achievement +
+								" on " + server + " for " + FactSQL.getInstance().getName(uuid) + " to " + progress);
+						return true;
+					}
+					else {
+						System.out.println("[FactAchievements] Failed to set progress of " + achievement +
+							" on " + server + " for " + FactSQL.getInstance().getName(uuid) + " to " + progress);
+						return false;
+					}
+					
+				});
 		
 	}
 	
-	public static boolean grantAchievement(UUID uuid, String server, String achievement) {
+	public static CompletableFuture<Boolean> grantAchievement(UUID uuid, String server, String achievement) {
 		return updateAchievement(uuid, server, achievement, "1/1");
 	}
-	public static boolean revokeAchievement(UUID uuid, String server, String achievement) {
+	public static CompletableFuture<Boolean> revokeAchievement(UUID uuid, String server, String achievement) {
 		return updateAchievement(uuid, server, achievement, "0/1");
 	}
 	
 	
 	public static String getAchievement(UUID uuid, String server, String achievement) {
-		
 		return FactSQL.getInstance().select(FactSQL.getAchievementsTable(), "PROGRESS", "`UUID`=? AND `SERVER`=? AND `NAME`=?",
 				new Object[] {uuid.toString(), server, achievement}).toString();
-		
 	}
 	
 	
 	
-	public static List<String[]> getAchievementList(UUID uuid, String server) {
+	public static CompletableFuture<List<String[]>> getAchievementList(UUID uuid, String server) {
 		
-		List<String[]> list = new ArrayList<String[]>();
+		return FactSQL.getInstance().select(FactSQL.getAchievementsTable(), new String[] {"NAME", "PROGRESS"},
+				"`UUID`=? AND `SERVER`=?", new Object[] {uuid.toString(), server}).thenApply((rows) -> {
+					
+					List<String[]> list = new ArrayList<String[]>();
+					
+					for (Map<String, Object> row : rows) {
+						list.add(new String[] {row.get("NAME").toString(), row.get("PROGRESS").toString()});
+					}
+					
+					return list;
+					
+				});
 		
-		List<Map<String, Object>> rows = FactSQL.getInstance().select(FactSQL.getAchievementsTable(), new String[] {"NAME", "PROGRESS"},
-				"`UUID`=? AND `SERVER`=?", new Object[] {uuid.toString(), server});
-		for (Map<String, Object> row : rows) {
-			list.add(new String[] {row.get("NAME").toString(), row.get("PROGRESS").toString()});
-		}
-		
-		return list;
 	}
 }
